b2c5fcbd8010435559cb67073e883d86979efecc
Jenn Harding <jenn.d.harding@gmail.com>
2020-05-22 16:27:12 -0400

    fixed dft arrays

--- DFT_Functions.py
+++ DFT_Functions.py
@@ -1,25 +1,25 @@
 
 from music21 import stream, note, pitch, chord, meter, corpus, converter
+import numpy as np
 
-import DFT_MultisetClass as MC
 import DFT_Corpus as CP
+from DFT_MultisetClass import dft_array
 
 
-def quantize_list(angle, quant=12):
-    """Quantizes the raw phase data to one of the 12 nodes (30 degrees apart).
+def quantize_array(array, quant=12):
+    """Quantizes the values of an array to the nearest of 12 "nodes."
 
     Arguments:
-        angle {float} -- raw phase data
+        array {numpy array} -- Numpy array with the phase values for each component
 
     Keyword Arguments:
-        quant {int} -- number of nodes on the component circle (default: {12})
+        quant {int} -- Number of nodes around the component circle (default: {12})
 
     Returns:
-        float -- quantized data
+        numpy array -- Numpy array with the quantized phase values for each component
     """
-    spacing = 360 / quant
-    q = angle/spacing
-    q = round(q)  
+    spacing = 360/quant
+    q = np.around(array/spacing)
     return q * spacing
   
 
@@ -164,7 +164,7 @@ def sliding_window(score, beat_offset_list, window_size, strategy, log=True, edo
     all_arrays = []
     for idx, window_begin in enumerate(beat_offset_list[:-window_size]):
         window_end = beat_offset_list[idx + window_size]
-        array = [0]*edo
+        current_array = np.array([0.0]*12)
         measure1 = get_measure_number(score=score, offset=window_begin)
         if window_end == beat_offset_list[-1]:
             measure2 = get_measure_number(score=score, offset=beat_offset_list[-2])
@@ -178,18 +178,18 @@ def sliding_window(score, beat_offset_list, window_size, strategy, log=True, edo
             
             if isinstance(elem, chord.Chord):
                 for a in elem.notes:
-                    array = update_array(
-                        array=array, 
+                    current_array = update_array(
+                        array=current_array, 
                         note_=a, 
                         strategy=strategy)
             elif isinstance(elem, note.Note):
-                array = update_array(
-                    array=array, 
+                current_array = update_array(
+                    array=current_array, 
                     note_=elem, 
                     strategy=strategy)
 
-        all_arrays.append(MC.MultiSetArray(
-            array=array, 
+        all_arrays.append(dft_array(
+            array=current_array, 
             log_weight=log, 
             measure_range=(measure1, measure2)))
         
--- DFT_MultisetClass.py
+++ DFT_MultisetClass.py
@@ -1,8 +1,7 @@
-import math
 import numpy as np
 
 
-class MultiSetArray(object):
+class dft_array(object):
     def __init__(self, array, measure_range=None, log_weight=True):
         self.original_array = array
         self.measure_range = measure_range
@@ -10,35 +9,23 @@ class MultiSetArray(object):
             self.start_measure = measure_range[0]
             self.end_measure = measure_range[1]
         self.log_weight = log_weight
-
-    def log_weighting(self):
-        return [math.log(element + 1, 2) for element in self.original_array]
     
-    def all_complex(self):
+    def do_dft(self):
         if self.log_weight is True:
-            return np.fft.fft(self.log_weighting())
+            return np.fft.fft(np.log2(self.original_array + 1))
         else:
             return np.fft.fft(self.original_array)
     
-    def all_magnitudes(self):
-        return list(np.abs(self.all_complex()))
+    def mag_dict(self):
+        return {f'f{i}' : np.abs(self.do_dft())[i] for i in range(1, 7)}
     
-    def all_phases(self):
-        return list(np.angle(self.all_complex(), deg=True))
+    def phase_dict(self):
+        return {f'f{i}' : np.angle(self.do_dft(), deg=True)[i] for i in range(1, 7)}
     
-    def make_mag_dict(self):
-        return {f'f{i}' : self.all_magnitudes()[i] for i in range(1, 7)}
+    def rounded_weighted_array(self):
+        return np.around(np.log2(self.original_array), decimals=2)
     
-    def make_phase_dict(self):
-        return {f'f{i}' : self.all_phases()[i] for i in range(1, 7)}
-    
-    def round_log_array(self):
-        return[round(x, 2) for x in self.log_weighting()]
-    
-    def round_original_array(self):
-        return [round(x, 2) for x in self.original_array]
-    
-    def __repr__(self):
-        return '<'+str([round(x, 2) for x in self.original_array])+'>'
-
+    def rounded_original_array(self):
+        return np.around(self.original_array, decimals=2)
+        
     
